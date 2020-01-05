27a268ea0236e126de26e0c79056fe9fb9c04f8e
tboyd4 <tboyd1898@gmail.com>
2020-01-04 20:35:47 -0600

    save buttons save events into local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,11 @@
+// This will check for and load anything from local storage before we do anything else
+
+
+
+
+
+
+
 // this function will grab the current date using moment.js and display it bold in the header
 function todayDate() {
   let todate = moment().format("LL");
@@ -35,8 +43,8 @@ function theFuture(classArg) {
 }
 
 // If Statement to assign color codes to inputs
-function setBackground() {
-  if (currentTime() === 9) {
+// function setBackground() {
+//   if (currentTime() === 9) {
     thePresent("nine-am");
     theFuture("ten-am");
     theFuture("elev-am");
@@ -46,97 +54,135 @@ function setBackground() {
     theFuture("three-pm");
     theFuture("four-pm");
     theFuture("five-pm");
-  } else if (currentTime() === 10) {
-    thePast("nine-am");
-    thePresent("ten-am");
-    theFuture("elev-am");
-    theFuture("twelve-pm");
-    theFuture("one-pm");
-    theFuture("two-pm");
-    theFuture("three-pm");
-    theFuture("four-pm");
-    theFuture("five-pm");
-  } else if (currentTime() === 11) {
-    thePast("nine-am");
-    thePast("ten-am");
-    thePresent("elev-am");
-    theFuture("twelve-pm");
-    theFuture("one-pm");
-    theFuture("two-pm");
-    theFuture("three-pm");
-    theFuture("four-pm");
-    theFuture("five-pm");
-  } else if (currentTime() === 12) {
-    thePast("nine-am");
-    thePast("ten-am");
-    thePast("elev-am");
-    thePresent("twelve-pm");
-    theFuture("one-pm");
-    theFuture("two-pm");
-    theFuture("three-pm");
-    theFuture("four-pm");
-    theFuture("five-pm");
-  } else if (currentTime() === 13) {
-    thePast("nine-am");
-    thePast("ten-am");
-    thePast("elev-am");
-    thePast("twelve-pm");
-    thePresent("one-pm");
-    theFuture("two-pm");
-    theFuture("three-pm");
-    theFuture("four-pm");
-    theFuture("five-pm");
-  } else if (currentTime() === 14) {
-    thePast("nine-am");
-    thePast("ten-am");
-    thePast("elev-am");
-    thePast("twelve-pm");
-    thePast("one-pm");
-    thePresent("two-pm");
-    theFuture("three-pm");
-    theFuture("four-pm");
-    theFuture("five-pm");
-  } else if (currentTime() === 15) {
-    thePast("nine-am");
-    thePast("ten-am");
-    thePast("elev-am");
-    thePast("twelve-pm");
-    thePast("one-pm");
-    thePast("two-pm");
-    thePreset("three-pm");
-    theFuture("four-pm");
-    theFuture("five-pm");
-  } else if (currentTime() === 16) {
-    thePast("nine-am");
-    thePast("ten-am");
-    thePast("elev-am");
-    thePast("twelve-pm");
-    thePast("one-pm");
-    thePast("two-pm");
-    thePast("three-pm");
-    thePresent("four-pm");
-    theFuture("five-pm");
-  } else if (currentTime() === 17) {
-    thePast("nine-am");
-    thePast("ten-am");
-    thePast("elev-am");
-    thePast("twelve-pm");
-    thePast("one-pm");
-    thePast("two-pm");
-    thePast("three-pm");
-    thePast("four-pm");
-    theFuture("five-pm");
-  } else if (currentTime() < 9 || currentTime() > 17) {
-    thePast("nine-am");
-    thePast("ten-am");
-    thePast("elev-am");
-    thePast("twelve-pm");
-    thePast("one-pm");
-    thePast("two-pm");
-    thePast("three-pm");
-    thePast("four-pm");
-    thePast("five-pm");
-  }
+//   } else if (currentTime() === 10) {
+//     thePast("nine-am");
+//     thePresent("ten-am");
+//     theFuture("elev-am");
+//     theFuture("twelve-pm");
+//     theFuture("one-pm");
+//     theFuture("two-pm");
+//     theFuture("three-pm");
+//     theFuture("four-pm");
+//     theFuture("five-pm");
+//   } else if (currentTime() === 11) {
+//     thePast("nine-am");
+//     thePast("ten-am");
+//     thePresent("elev-am");
+//     theFuture("twelve-pm");
+//     theFuture("one-pm");
+//     theFuture("two-pm");
+//     theFuture("three-pm");
+//     theFuture("four-pm");
+//     theFuture("five-pm");
+//   } else if (currentTime() === 12) {
+//     thePast("nine-am");
+//     thePast("ten-am");
+//     thePast("elev-am");
+//     thePresent("twelve-pm");
+//     theFuture("one-pm");
+//     theFuture("two-pm");
+//     theFuture("three-pm");
+//     theFuture("four-pm");
+//     theFuture("five-pm");
+//   } else if (currentTime() === 13) {
+//     thePast("nine-am");
+//     thePast("ten-am");
+//     thePast("elev-am");
+//     thePast("twelve-pm");
+//     thePresent("one-pm");
+//     theFuture("two-pm");
+//     theFuture("three-pm");
+//     theFuture("four-pm");
+//     theFuture("five-pm");
+//   } else if (currentTime() === 14) {
+//     thePast("nine-am");
+//     thePast("ten-am");
+//     thePast("elev-am");
+//     thePast("twelve-pm");
+//     thePast("one-pm");
+//     thePresent("two-pm");
+//     theFuture("three-pm");
+//     theFuture("four-pm");
+//     theFuture("five-pm");
+//   } else if (currentTime() === 15) {
+//     thePast("nine-am");
+//     thePast("ten-am");
+//     thePast("elev-am");
+//     thePast("twelve-pm");
+//     thePast("one-pm");
+//     thePast("two-pm");
+//     thePreset("three-pm");
+//     theFuture("four-pm");
+//     theFuture("five-pm");
+//   } else if (currentTime() === 16) {
+//     thePast("nine-am");
+//     thePast("ten-am");
+//     thePast("elev-am");
+//     thePast("twelve-pm");
+//     thePast("one-pm");
+//     thePast("two-pm");
+//     thePast("three-pm");
+//     thePresent("four-pm");
+//     theFuture("five-pm");
+//   } else if (currentTime() === 17) {
+//     thePast("nine-am");
+//     thePast("ten-am");
+//     thePast("elev-am");
+//     thePast("twelve-pm");
+//     thePast("one-pm");
+//     thePast("two-pm");
+//     thePast("three-pm");
+//     thePast("four-pm");
+//     theFuture("five-pm");
+//   } else if (currentTime() < 9 || currentTime() > 17) {
+//     thePast("nine-am");
+//     thePast("ten-am");
+//     thePast("elev-am");
+//     thePast("twelve-pm");
+//     thePast("one-pm");
+//     thePast("two-pm");
+//     thePast("three-pm");
+//     thePast("four-pm");
+//     thePast("five-pm");
+//   }
+// }
+
+// setBackground();
+
+// function to save input value into local storage
+
+function saveEvent (timeSlot) {
+    let saveSlot = $("#"+timeSlot).val();
+    console.log(saveSlot);
+    localStorage.setItem(timeSlot, saveSlot);
 }
 
-setBackground();
+// event listeners for the save buttons
+
+$("#nine-btn").on("click", function () {
+    saveEvent("nine-am");
+});
+$("#ten-btn").on("click", function () {
+    saveEvent("ten-am");
+});
+$("#elev-btn").on("click", function () {
+    saveEvent("elev-am");
+});
+$("#twelve-btn").on("click", function () {
+    saveEvent("twelve-pm");
+});
+$("#one-btn").on("click", function () {
+    saveEvent("one-pm");
+});
+$("#two-btn").on("click", function () {
+    saveEvent("two-pm");
+});
+$("#three-btn").on("click", function () {
+    saveEvent("three-pm");
+});
+$("#four-btn").on("click", function () {
+    saveEvent("four-pm");
+});
+$("#five-btn").on("click", function () {
+    saveEvent("five-pm");
+});
