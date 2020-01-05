6ea40b802bc7942098b707140787ea945996c241
tboyd4 <tboyd1898@gmail.com>
2020-01-04 19:49:15 -0600

    background will now change depending on time of day

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,11 +6,12 @@ function todayDate() {
 }
 
 todayDate();
-
 // this function will grab the current time of day. It will be military time for ease of use later
 function currentTime() {
-  let theTime = moment().format("HH:mm");
+  let theTime = moment().format("HH");
   console.log(theTime);
+
+  return theTime;
 }
 
 currentTime();
@@ -32,6 +33,99 @@ function theFuture(classArg) {
   $("#" + classArg).addClass("text-white");
 }
 
-thePast("nine-am");
-thePresent("ten-am");
-theFuture("twelve-pm");
+// If Statement to assign color codes to inputs
+function setBackground() {
+  if (currentTime() === 9) {
+    thePresent("nine-am");
+    theFuture("ten-am");
+    theFuture("elev-am");
+    theFuture("twelve-pm");
+    theFuture("one-pm");
+    theFuture("two-pm");
+    theFuture("three-pm");
+    theFuture("four-pm");
+    theFuture("five-pm");
+  } else if (currentTime() === 10) {
+    thePast("nine-am");
+    thePresent("ten-am");
+    theFuture("elev-am");
+    theFuture("twelve-pm");
+    theFuture("one-pm");
+    theFuture("two-pm");
+    theFuture("three-pm");
+    theFuture("four-pm");
+    theFuture("five-pm");
+  } else if (currentTime() === 11) {
+    thePast("nine-am");
+    thePast("ten-am");
+    thePresent("elev-am");
+    theFuture("twelve-pm");
+    theFuture("one-pm");
+    theFuture("two-pm");
+    theFuture("three-pm");
+    theFuture("four-pm");
+    theFuture("five-pm");
+  } else if (currentTime() === 12) {
+    thePast("nine-am");
+    thePast("ten-am");
+    thePast("elev-am");
+    thePresent("twelve-pm");
+    theFuture("one-pm");
+    theFuture("two-pm");
+    theFuture("three-pm");
+    theFuture("four-pm");
+    theFuture("five-pm");
+  } else if (currentTime() === 13) {
+    thePast("nine-am");
+    thePast("ten-am");
+    thePast("elev-am");
+    thePast("twelve-pm");
+    thePresent("one-pm");
+    theFuture("two-pm");
+    theFuture("three-pm");
+    theFuture("four-pm");
+    theFuture("five-pm");
+  } else if (currentTime() === 14) {
+    thePast("nine-am");
+    thePast("ten-am");
+    thePast("elev-am");
+    thePast("twelve-pm");
+    thePast("one-pm");
+    thePresent("two-pm");
+    theFuture("three-pm");
+    theFuture("four-pm");
+    theFuture("five-pm");
+  } else if (currentTime() === 15) {
+    thePast("nine-am");
+    thePast("ten-am");
+    thePast("elev-am");
+    thePast("twelve-pm");
+    thePast("one-pm");
+    thePast("two-pm");
+    thePreset("three-pm");
+    theFuture("four-pm");
+    theFuture("five-pm");
+  } else if (currentTime() === 16) {
+    thePast("nine-am");
+    thePast("ten-am");
+    thePast("elev-am");
+    thePast("twelve-pm");
+    thePast("one-pm");
+    thePast("two-pm");
+    thePast("three-pm");
+    thePresent("four-pm");
+    theFuture("five-pm");
+  } else if (currentTime() === 17) {
+    thePast("nine-am");
+    thePast("ten-am");
+    thePast("elev-am");
+    thePast("twelve-pm");
+    thePast("one-pm");
+    thePast("two-pm");
+    thePast("three-pm");
+    thePast("four-pm");
+    theFuture("five-pm");
+  }
+}
+
+setBackground();
