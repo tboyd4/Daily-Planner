7476d87bd669e06024939c63583546d3cc6c32aa
tboyd4 <tboyd1898@gmail.com>
2020-01-04 19:15:02 -0600

    three functions to change background color pending time are on

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,8 +9,29 @@ todayDate();
 
 // this function will grab the current time of day. It will be military time for ease of use later
 function currentTime() {
-    let theTime = moment().format("HH:mm")
-    console.log(theTime);
+  let theTime = moment().format("HH:mm");
+  console.log(theTime);
 }
 
 currentTime();
+
+//these three functions will set the background color of the input according to the time of day. Past, Present, Future
+
+function thePast(classArg) {
+  $("#" + classArg).addClass("bg-secondary");
+  $("#" + classArg).addClass("text-white");
+}
+
+function thePresent(classArg) {
+  $("#" + classArg).addClass("bg-primary");
+  $("#" + classArg).addClass("text-white");
+}
+
+function theFuture(classArg) {
+  $("#" + classArg).addClass("bg-success");
+  $("#" + classArg).addClass("text-white");
+}
+
+thePast("nine-am");
+thePresent("ten-am");
+theFuture("twelve-pm");
